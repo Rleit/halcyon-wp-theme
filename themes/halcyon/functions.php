83f3834967cedee0226f8d0b8f29256f0e4d99d7
Rainer Leit <rleit@uclan.ac.uk>
2018-04-10 17:55:37 +0100

    Boostrap ++

--- themes/halcyon/functions.php
+++ themes/halcyon/functions.php
@@ -100,6 +100,11 @@ function html5blank_header_scripts()
 
         wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
         wp_enqueue_script('html5blankscripts'); // Enqueue it!
+
+        wp_register_script('bootstrap_js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '4.1.0'); // Bootstrap
+        wp_enqueue_script('bootstrap_js'); // Enqueue it!
+
+        
     }
 }
 
@@ -109,6 +114,8 @@ function html5blank_conditional_scripts()
     if (is_page('pagenamehere')) {
         wp_register_script('scriptname', get_template_directory_uri() . '/js/scriptname.js', array('jquery'), '1.0.0'); // Conditional script(s)
         wp_enqueue_script('scriptname'); // Enqueue it!
+
+        
     }
 }
 
@@ -118,8 +125,13 @@ function html5blank_styles()
     wp_register_style('normalize', get_template_directory_uri() . '/normalize.css', array(), '1.0', 'all');
     wp_enqueue_style('normalize'); // Enqueue it!
 
+    wp_register_style('boostrap_css', get_template_directory_uri() . '/css/bootstrap.css', array(), '4.1.0', 'all');
+    wp_enqueue_style('boostrap_css'); // Enqueue it!
+
     wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
     wp_enqueue_style('html5blank'); // Enqueue it!
+
+    
 }
 
 // Register HTML5 Blank Navigation
