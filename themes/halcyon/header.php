ae8974e4a63a99944f37b1fdad5d0ca6c2a3d3a3
Rainer Leit <rleit@uclan.ac.uk>
2018-04-10 22:00:47 +0100

    1

--- themes/halcyon/header.php
+++ themes/halcyon/header.php
@@ -1,35 +1,42 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js">
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title>
+		<?php wp_title(''); ?>
+		<?php if(wp_title('', false)) { echo ' :'; } ?>
+		<?php bloginfo('name'); ?>
+	</title>
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+	<link href="//www.google-analytics.com" rel="dns-prefetch">
+	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php bloginfo('description'); ?>">
 
-	</head>
-	<body <?php body_class(); ?>>
+	<?php wp_head(); ?>
+	<script>
+		// conditionizr.com
+		// configure environment tests
+		conditionizr.config({
+			assets: '<?php echo get_template_directory_uri(); ?>',
+			tests: {}
+		});
+	</script>
 
-		<!-- wrapper -->
-		<div class="wrapper">
+</head>
 
+<body <?php body_class(); ?>>
+
+	<!-- wrapper -->
+	<div class="container-fluid" >
+		<div class="row full-width-row">
 			<!-- header -->
-			<header class="header clear" role="banner">
+			<div class="col-md-3 col-lg-2">
+				<header class="header clear h-100" role="banner">
 
 					<!-- logo -->
 					<div class="logo">
@@ -42,9 +49,24 @@
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+
+
+						<div class="row w-100 ">
+							<div class="col-md-4 w-25">
+							</div>
+							<div class="col-md-4 w-50 text-center">
+								<?php html5blank_nav(); ?>
+							</div>
+							<div class="col-md-4 w-25">
+							</div>
+						</div>
 					</nav>
 					<!-- /nav -->
 
-			</header>
-			<!-- /header -->
+					<?php get_footer(); ?>
+
+				</header>
+
+				
+
+				<!-- /header -->
--- themes/halcyon/index.php
+++ themes/halcyon/index.php
@@ -1,19 +1,25 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+		<div class="col-xs-12 col-md-6 col-lg-10">
+				<!-- section -->
+				<div class="container-fluid">
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+					<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
+					<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
+					<?php get_template_part('pagination'); ?>
 
-		</section>
-		<!-- /section -->
-	</main>
+					<?php /* get_sidebar(); */ ?>
 
-<?php get_sidebar(); ?>
+				</div>
+				<!-- /section -->
 
-<?php get_footer(); ?>
+		</div>
+	</div>
+
+</div>
+
+
+
+
